1c69e349fe8a450e86e6f9ac91278982af1bbe44
omesh139 <119126407+omesh139@users.noreply.github.com>
2025-06-27 07:09:56 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "blob:https://imgur.com/ecd6d182-c1d1-41c7-90a7-056e1e45f9dc",
               },
               caption: sid,
             });
